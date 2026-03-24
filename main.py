b97b7c42dceeeb3d3dd11e802c55e2ce5de2f639
Misha Koroliovas <koroliov1985@gmail.com>
2026-03-24 22:58:39 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from pynput import keyboard
+import logging
+import datetime
+
+
+print(3)
+
+logging.basicConfig(
+    filename="keylog.txt",
+    level = logging.DEBUG,
+    format = "%(asctime)s - %(message)s"
+)
